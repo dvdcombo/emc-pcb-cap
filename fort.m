a433d2fef7001376cfdf2a0e3d60c919b57cad02
Ahmet Furkan YILMAZ <dvdcombo@users.noreply.github.com>
2017-05-30 17:46:30 +0300

    220

--- fort.m
+++ fort.m
@@ -271,20 +271,6 @@ for I=1:N;
             end
 
        end
-end
-%C******POTENTIAL WITHOUT THE BOARD ************************************ 
-function [DP,DM,PHI]=PHI(D,FAC,DELTA,ER,T)
-
-	 DP=D+DELTA/2
-	 DM=D-DELTA/2
-	 
-	 if(D==0) PHI=-DELTA*FAC*(ALOG(DELTA/2)-1) 
-	 end
-	 else
-	 PHI=FAC*(DM*ALOG(DM)-DP*ALOG(DP)+DELTA);
-	 
-	 end
-end
-      
+end 
       
       
